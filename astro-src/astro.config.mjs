de65a1627bcc69716fe4ebba42943b23eb42e7e8
0xk1f0 <fteppan@protonmail.com>
2023-03-10 11:44:24 +0100

    astro: add svelte and deno

--- astro-src/astro.config.mjs
+++ astro-src/astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+  output: "server",
+  adapter: deno()
+});

7d127b50db7276888a2de3c6d377ef33f044386d
0xk1f0 <fteppan@protonmail.com>
2023-06-09 17:27:30 +0200

    add tailwind integration and cleanup

--- astro-src/astro.config.mjs
+++ astro-src/astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import deno from "@astrojs/deno";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), tailwind()],
   output: "server",
   adapter: deno(),
   publicDir: './src/public'
